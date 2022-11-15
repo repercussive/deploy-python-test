7ae93aa2c4d36ea08e2476a5c88dee17e7ab7159
Alex Thomas <92339139+AJT8112@users.noreply.github.com>
2022-11-15 16:15:58 +0000

    Initial commit

---
+++ ImportTime.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import ntplib
+from time import ctime
+app = Flask(__name__)
+@app.route('/')
+
+def import_time():
+
+    c = ntplib.NTPClient()
+    response = c.request('europe.pool.ntp.org', version=3)
+    mytime=ctime(response.tx_time)
+    print(mytime)  ##  if you want to see the time it displays
+    return render_template('index.html', time = mytime)
+
+if __name__ == '__main__':
+    app.run()
